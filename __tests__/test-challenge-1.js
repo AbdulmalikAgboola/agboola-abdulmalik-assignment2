5701bb7947cfe61fcb96f9eb1d9cbb43afd122b4
Abdulmalik Agboola <agboolaabdulmalik@gmail.com>
2022-08-18 14:30:50 +0100

    removed the negative signs

--- __tests__/test-challenge-1.js
+++ __tests__/test-challenge-1.js
@@ -6,25 +6,25 @@ const maybe = sumOfNumbers === undefined ? test.skip : test;
 maybe('Challenge - 1 : my sumNumbers function can sum an array of numbers', () => {
     const numbers = [
         68,
-        -68,
+        68,
         27,
         94,
         72,
-        -25,
-        -51,
+        25,
+        51,
         32,
         10,
         64,
-        -94,
+        94,
         4,
         34,
-        -86,
+        86,
         90,
         81,
         20,
-        -56,
-        -91,
-        -50
+        56,
+        91,
+        50
     ];
     expect(sumOfNumbers(numbers)).toBe(75);
 });
